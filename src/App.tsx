cab9ecb60b33d2241923bbd23aaac500d5f9fb42
pajecawav <pajecawav@gmail.com>
2022-05-27 21:22:07 +0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { useState } from "preact/hooks";
 import { Breadcrumb } from "./components/Breadcrumb";
 import { FileTree } from "./components/FileTree";
 import { LocsTree } from "./components/LocsTree";
 import { useLocs } from "./hooks/useLocs";
 
-export function App() {
+interface AppProps {
+	path: string;
+}
+
+export function App({ path: pathname }: AppProps) {
 	const locs = useLocs();
 
-	const [path, setPath] = useState<string[]>(["pajecawav", "pockly", "test"]);
+	const path = pathname.split("/");
 
 	return (
 		<div className="max-w-2xl mx-auto p-2 flex flex-col gap-2">
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { render } from "preact";
+import Router, { Route } from "preact-router";
 import { App } from "./App";
 import "./index.css";
 
-render(<App />, document.getElementById("app")!);
+function Main() {
+	return (
+		<Router>
+			<Route path="/:path*" component={App} />
+		</Router>
+	);
+}
+
+render(<Main />, document.getElementById("app")!);
