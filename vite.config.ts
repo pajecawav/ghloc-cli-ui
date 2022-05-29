0bd07217d22b061ae421746cb35b4b81aec0aa4b
pajecawav <pajecawav@gmail.com>
2022-05-29 23:17:06 +0300

    fix path rewrite in dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,10 @@ export default defineConfig({
 			"/api": {
 				// use hard-coded url for ghloc server in dev
 				target: "http://localhost:8080/pajecawav/ghloc-cli-ui/master",
-				rewrite: () => "",
+				rewrite: path => {
+					const url = new URL(path, "http://localhost:3000");
+					return url.search;
+				},
 			},
 		},
 	},
